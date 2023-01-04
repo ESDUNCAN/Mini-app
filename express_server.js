36af8c7ee4e74a45f4abdec1b19748342ad953b2
ESDUNCAN <102484574+ESDUNCAN@users.noreply.github.com>
2023-01-04 15:15:17 -0500

    create and implement generate Random string

--- express_server.js
+++ express_server.js
@@ -4,11 +4,22 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString(length) {
+    let randomString = "";
+    let characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++){
+        randomString += characters.charAt(Math.floor(Math.random() * (characters.length)))
+    }
+    return randomString
+}
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,7 +27,7 @@ app.get("/", (req, res) => {
 
 app.get("/urls.json", (req, res) => {
     res.json(urlDatabase);
-  });
+});
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
@@ -25,12 +36,22 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
     const templateVars = { urls: urlDatabase };
     res.render("urls_index", templateVars);
-  });
+});
+
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+});
 
 app.get("/urls/:id", (req, res) => {
     const templateVars = { id: req.params.id, longURL: req.params.longURL };
     res.render("urls_show", templateVars);
+});
+
+app.post("/urls", (req, res) => {
+    console.log(req.body); 
+    res.send(generateRandomString(5)); 
   });
+  
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
